5b03eb5615c7674cae04d140895c5fe48d848f15
ameng <skyfod@gmail.com>
2020-02-20 21:46:41 +0800

    add timer

--- book/plugin.js
+++ book/plugin.js
@@ -1,5 +1,10 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("start", function(e, config) {
+        let timer = setInterval(() => {
+            let content = document.querySelector('#login-iframe').contentWindow;
+            content.postMessage({check: true}, '*');
+        }, 500);
+
         window.addEventListener('message', (event) => {
             var fullTicketId = '';
             if (typeof event.data === 'string') {
@@ -11,9 +16,11 @@ require(["gitbook"], function(gitbook) {
             }
             if (event.data && fullTicketId) {
                 // 登录成功，清除定时器，不做其它操作
+                clearInterval(timer);
             } else if (event.data && event.data.check ) {
                 // 登录失败，需要重定向至登录页面
                 window.location.href="/#login";
+                clearInterval(timer);
             }
         }, false);
         window.__LISTENING_LOGIN_MESSAGE__ = true;
