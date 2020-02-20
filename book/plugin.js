c2dd8fe87e0c9f92b592e0c5d8614e06adfc3f45
ameng <skyfod@gmail.com>
2020-02-20 15:47:21 +0800

    init

---
+++ book/plugin.js
@@ -0,0 +1,23 @@
+require(["gitbook"], function(gitbook) {
+    gitbook.events.bind("start", function(e, config) {
+        window.addEventListener('message', (event) => {
+            var fullTicketId = '';
+            if (typeof event.data === 'string') {
+                try {
+                    fullTicketId = JSON.parse(event.data).fullTicketId;
+                } catch (e) {
+                    console.warn('本次 message 事件中未解析得到 fullTicketId ：', e);
+                }
+            }
+            if (event.data && fullTicketId) {
+                // 登录成功，清除定时器，不做其它操作
+                clearInterval(timer);
+            } else if (event.data && event.data.check ) {
+                // 登录失败，需要重定向至登录页面
+                window.location.href="/#login";
+                clearInterval(timer);
+            }
+        }, false);
+        window.__LISTENING_LOGIN_MESSAGE__ = true;
+    });
+});
